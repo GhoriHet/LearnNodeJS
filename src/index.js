2a4d5ca1e3b745bc69447a81a23729399689ac3c
GhoriHet <ghorihet71@gmail.com>
2024-02-05 14:14:01 +0530

    Node Work Done!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const rect = require("./rectangle");
+const employee = require("./Employee")
 
 //1.
 // console.log(rect.area(10, 20));
@@ -17,15 +18,27 @@ const rect = require("./rectangle");
 // solveRectangle(10, 20)
 
 //3.
-const solveRectangle = (l, w) => {
-    rect.rectOperation(l, w, (err, obj) => {
+// const solveRectangle = (l, w) => {
+//     rect.rectOperation(l, w, (err, obj) => {
+//         if (err) {
+//             console.log(err.message);
+//         } else {
+//             console.log("Area of circle is: " + obj.area());
+//             console.log("Area of circle is: " + obj.perimeter());
+//         }
+//     })
+// }
+
+// solveRectangle(10, -20)
+
+const totalSalary = (salary) => {
+    employee.employeeSalary(salary, (err, bonus) => {
         if (err) {
-            console.log(err.message);
+            console.error(err.message);
         } else {
-            console.log("Area of circle is: " + obj.area());
-            console.log("Area of circle is: " + obj.perimeter());
+            console.log(bonus);
         }
     })
 }
 
-solveRectangle(10, -20)
+totalSalary(31000)
