fce4ce899f629174e7bb477fa6f6ab365e30bc83
GhoriHet <ghorihet71@gmail.com>
2024-02-05 09:43:43 +0530

    First Lecture Node

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const rect = require("./rectangle");
+
+//1.
+// console.log(rect.area(10, 20));
+// console.log(rect.perimeter(10, 30));
+
+//2.
+// const solveRectangle = (l, w) => {
+//     if (l <= 0 || w <= 0) {
+//         console.log(new Error("Length and width must be greter than zero."));
+//     } else {
+//         console.log(rect.area(l, w));
+//         console.log(rect.perimeter(l, w));
+//     }
+// }
+
+// solveRectangle(10, 20)
+
+//3.
+const solveRectangle = (l, w) => {
+    rect.rectOperation(l, w, (err, obj) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log("Area of circle is: " + obj.area());
+            console.log("Area of circle is: " + obj.perimeter());
+        }
+    })
+}
+
+solveRectangle(10, -20)
