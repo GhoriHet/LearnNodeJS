3c03b55d057b3f634d12c8223644954a308c9c26
GhoriHet <ghorihet71@gmail.com>
2024-03-14 08:27:16 +0530

    INSERT QUERY DONE

--- src/db/mysql.db.js
+++ src/db/mysql.db.js
@@ -1,25 +1,25 @@
 const MYSQL = require('mysql2')
 
-// Create the connection pool. The pool-specific settings are the defaults
 const pool = MYSQL.createPool({
     host: 'localhost',
     user: 'root',
     database: 'practical',
     waitForConnections: true,
     connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    maxIdle: 10,
+    idleTimeout: 60000,
     queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0
 });
 
-pool.query(`select * from orders`, (error, result, fields) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log(result)
-    return console.log("MYSQL database connected!");
-})
+// pool.query(`select * from orders`, (error, result, fields) => {
+//     if (error) {
+//         return console.log(error);
+//     }
+//     console.log(result)
+//     return console.log("MYSQL database connected!");
+// })
 
-module.exports = pool;
+const poolPromise = pool.promise();
+module.exports = poolPromise;
--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,39 +1,56 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const productsSchema = new mongoose.Schema({
-    _id: {
-        type: Number
-    },
-    subcategory_id: {
-        // type: mongoose.Types.ObjectId,
-        type: Number,
-        ref: 'Subcategories',
-        required: true
-    },
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    descrription: {
-        type: String,
-        trim: true
-    },
-    discount: {
-        type: Number,
-    },
-    // image: {
-    //     type: String,
-    //     required: true
-    // },
-    isActive: {
-        type: Boolean,
-        default: true
+const poolPromise = require("../db/mysql.db");
+
+// const productsSchema = new mongoose.Schema({
+//     _id: {
+//         type: Number
+//     },
+//     subcategory_id: {
+//         // type: mongoose.Types.ObjectId,
+//         type: Number,
+//         ref: 'Subcategories',
+//         required: true
+//     },
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     descrription: {
+//         type: String,
+//         trim: true
+//     },
+//     discount: {
+//         type: Number,
+//     },
+//     // image: {
+//     //     type: String,
+//     //     required: true
+//     // },
+//     isActive: {
+//         type: Boolean,
+//         default: true
+//     }
+// }, {
+//     timestamps: true,
+//     versionKey: false
+// });
+
+// const Products = mongoose.model("Products", productsSchema);
+// module.exports = Products;
+
+const insertProduct = async (data) => {
+    try {
+        let sqlQuery = 'INSERT INTO salespeople	(snum, sname, city, comm) VALUES (?, ?, ?, ?)';
+        let res = poolPromise.execute(sqlQuery, [data.snum, data.sname, data.city, data.comm])
+
+        console.log(res)
+    } catch (error) {
+        console.log(error.message)
     }
-}, {
-    timestamps: true,
-    versionKey: false
-});
+}
 
-const Products = mongoose.model("Products", productsSchema);
-module.exports = Products;
+module.exports = {
+    insertProduct
+}
