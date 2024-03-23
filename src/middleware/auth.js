ae5d0dc193b79484d1b3adf1e66d9cc74df232af
ItalianStich <italianstitch77@gmail.com>
2024-03-23 11:03:57 +0530

    Google isAuthenticated done and authenticated to show category data

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,67 +1,90 @@
-const jwt = require('jsonwebtoken');
-const Users = require('../models/users.model');
+const jwt = require("jsonwebtoken");
+const Users = require("../models/users.model");
 
 const auth = (roles) => async (req, res, next) => {
+    console.log("first", req.isAuthenticated());
     if (req.isAuthenticated()) {
+        console.log("google", req.session, req.user);
         try {
-            const user = req.user;
-            const userRole = user.role;
+            const user = await Users.findById(req.user._id);
 
-            console.log("User Role:", userRole);
-            console.log("Roles Array:", roles);
+            const access_token = jwt.sign(
+                {
+                    _id: req.user._id,
+                    name: user.name,
+                    role: user.role,
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+            );
+            const refresh_token = jwt.sign(
+                {
+                    _id: req.user._id,
+                    name: user.name,
+                    role: user.role,
+                },
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+            );
 
-            const userRoles = roles.some((value) => value === userRole);
+            user.refresh_token = refresh_token;
+            await user.save({ validateBeforeSave: false });
 
-            console.log("User Roles Matched:", userRoles);
+            const options = {
+                httpOnly: true,
+                secure: true,
+            };
 
-            if (userRoles) {
-                return res.status(400).json({ message: "Access Denied!!" })
-            }
-            res.json({
-                success: true,
-                message: "Access Done!"
-            });
+            res.cookie("access_token", access_token, options);
+            res.cookie("refresh_token", refresh_token, options);
 
-            next()
+            next();
         } catch (error) {
             console.error("Error:", error);
-            return res.status(500).json({ message: "Internal server error: Authentication" });
+            return res
+                .status(500)
+                .json({ message: "Internal server error: Authentication" });
         }
-
-
     } else {
         try {
-            const token = req.cookies?.access_token || req.header("Authorization")?.replace("Bearer", "");
+            const token =
+                req.cookies?.access_token ||
+                req.header("Authorization")?.replace("Bearer", "");
 
             if (!token) {
-                res.status(401).json({ message: "Token not available." })
+                res.status(401).json({ message: "Token not available." });
             }
 
             try {
-                const validateToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+                const validateToken = await jwt.verify(
+                    token,
+                    process.env.ACCESS_TOKEN_SECRET
+                );
 
-                const user = await Users.findById(validateToken._id).select("-password -refresh_token");
+                const user = await Users.findById(validateToken._id).select(
+                    "-password -refresh_token"
+                );
 
                 if (!user) {
-                    res.status(404).json({ message: "User not found." })
+                    res.status(404).json({ message: "User not found." });
                 }
 
                 if (!roles.some((value) => value === user.role)) {
-                    return res.status(400).json({ message: "You have not permission." })
+                    return res.status(400).json({ message: "You have not permission." });
                 }
 
                 req.user = user;
-
             } catch (error) {
-                res.status(401).json({ message: "Invalid Token." })
+                res.status(401).json({ message: "Invalid Token." });
             }
-
         } catch (error) {
-            res.status(500).json({ message: "Internal server error: In Authentication" })
+            res
+                .status(500)
+                .json({ message: "Internal server error: In Authentication" });
         }
     }
-}
+};
 
 module.exports = {
-    auth
-}
+    auth,
+};
