712ed47b8c9d61f70b29310e8b92427e814cc8f0
GhoriHet <ghorihet71@gmail.com>
2024-04-02 08:55:23 +0530

    Group chat done

--- src/utils/socket.io.js
+++ src/utils/socket.io.js
@@ -10,15 +10,19 @@ const connectSocket = () => {
     });
 
     io.on("connection", (socket) => {
-        console.log('User connected:', socket.id);
+        // console.log('User connected:', socket.id);
 
         io.emit("send-message", "My event is occured!") //all
         // socket.broadcast.emit("send-message", "My event is occured!") //all exept itself
 
-        socket.on("message", ({room, message}) => {
-            console.log({room, message});
+        socket.on("message", ({ room, message }) => {
 
             io.to(room).emit("receive-message", message)
+        });
+
+        socket.on("join_group", (groupID, message) => {
+            socket.join(groupID)
+            io.to(groupID).emit("join_group", message)
         })
     });
 
